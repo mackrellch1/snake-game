a9e8bc299df8126a40f2033509f388f090e05d1f
Colin M <colin.mackrell99@gmail.com>
2021-11-13 22:29:05 -0500

    Added Multiplayer functionality

--- frontend/index.js
+++ frontend/index.js
@@ -3,37 +3,44 @@ const SNAKE_COLOR = '#c2c2c2'
 const FOOD_COLOR = '#e66917'
 const gameScreen = document.getElementById('gameScreen')
 
-const socket = io('http://192.168.1.240:2222')
+const socket = io('http://localhost:3000')
 
 
 socket.on('init', handleInit)
+socket.on('gameState', handleGameState)
+socket.on('gameOver', handleGameOver)
+socket.on('gameCode', handleGameCode)
+socket.on('unknownGame', handleUnknownGame)
+socket.on('tooManyPlayers', handleTooManyPlayers)
 
-let canvas, ctx
+const initialScreen = document.getElementById('initialScreen')
+const newGameBtn = document.getElementById('newGameButton')
+const joinGameBtn = document.getElementById('joinGameButton')
+const gameCodeInput = document.getElementById('gameCodeInput')
+const gameCodeDisplay = document.getElementById('gameCodeDisplay')
 
-const gameState = {
-    player: {
-        pos: {
-            x: 3,
-            y: 10,
-        },
-        vel: {
-            x: 1,
-            y: 0,
-        },
-        snake: [
-            {x: 1, y:10},
-            {x: 2, y:10},
-            {x: 3, y:10},
-        ],
-    },
-    food: {
-        x: 10,
-        y: 7,
-    },
-    gridsize: 20,
+newGameBtn.addEventListener('click', newGame)
+joinGameBtn.addEventListener('click', joinGame)
+
+function newGame() {
+    socket.emit('newGame')
+    init()
 }
 
+function joinGame() {
+    const gameCode = gameCodeInput.value
+    socket.emit('joinGame', gameCode)
+    init()
+}
+
+let canvas, ctx
+let playerNumber
+let gameActive = false
+
 function init() {
+    initialScreen.style.display = 'none'
+    gameScreen.style.display = 'block'
+
     canvas = document.getElementById('canvas')
     ctx = canvas.getContext('2d')
 
@@ -43,13 +50,14 @@ function init() {
     ctx.fillRect(0, 0, canvas.width, canvas.height)
 
     document.addEventListener('keydown', keydown)
+    gameActive = true
 }
 
 function keydown(e) {
-    console.log(e.keyCode)
+    socket.emit('keydown', e.keyCode)
+    
 }
 
-init()
 
 function paintGame(state) {
     ctx.fillStyle = BG_COLOR
@@ -62,7 +70,8 @@ function paintGame(state) {
     ctx.fillStyle = FOOD_COLOR
     ctx.fillRect(food.x * size, food.y * size, size, size)
 
-    paintPlayer(state.player, size, SNAKE_COLOR)
+    paintPlayer(state.players[0], size, SNAKE_COLOR)
+    paintPlayer(state.players[1], size, 'red')
 }
 
 function paintPlayer(playerState, size, color) {
@@ -74,8 +83,52 @@ function paintPlayer(playerState, size, color) {
     }
 }
 
-paintGame(gameState)
 
-function handleInit(msg) {
-    console.log(msg)
+
+function handleInit(number) {
+    playerNumber = number
+}
+
+function handleGameState(gameState) {
+    if(!gameActive) {
+        return
+    }
+    gameState = JSON.parse(gameState)
+    requestAnimationFrame(() => paintGame(gameState))
+}
+
+function handleGameOver(data) {
+    if(!gameActive) {
+        return
+    }
+    data = JSON.parse(data)
+
+    if(data.winner === playerNumber) {
+        alert("You Win!")
+    } else {
+        alert("YOU LOSE")
+    }
+    gameActive = false
+}
+
+function handleGameCode(gameCode) {
+    gameCodeDisplay.innerText = gameCode
+}
+
+function handleUnknownGame() {
+    reset()
+    alert('Unknown game code')
+}
+
+function handleTooManyPlayers() {
+    reset()
+    alert('This game is already in progress')
+}
+
+function reset() {
+    playerNumber = null
+    gameCodeInput.value = ""
+    gameCodeDisplay.innerText = ""
+    initialScreen.style.display = "block"
+    gameScreen.style.display = "none"
 }
